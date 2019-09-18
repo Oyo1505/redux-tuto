a694d11c50e710adbe198fd94183ee12e42d3e8d
RIGOULET Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2019-09-18 19:52:24 +0200

    fff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './component/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    	<Header />
     </div>
   );
 }
